6b6a4bb840d530bee8194f928a82f115035da155
voyage4301 <815717711@qq.com>
2019-01-30 16:42:16 +0800

    touch-move-demo

---
+++ index.js
@@ -0,0 +1,43 @@
+$(() => {
+    var $box = $('.box')
+
+    var pointA = { x: 0, y: 0 },
+        pointB = { x: 0, y: 0 },
+        juliA = 0,
+        juliB = 0
+
+    $box.on('touchstart', e => {
+        if (e.targetTouches.length < 1) {
+            return false
+        }
+        pointA.x = e.targetTouches[0].pageX
+        pointA.y = e.targetTouches[0].pageY
+        pointB.x = e.targetTouches[1].pageX
+        pointB.y = e.targetTouches[1].pageY
+        juliA = Math.pow((Math.pow(Math.abs(pointA.x - pointB.x)) + Math.pow(Math.abs(pointA.y - pointB.y))), .5)
+    })
+
+    $box.on('touchmove', e => {
+        if (e.targetTouches.length < 1) {
+            return false
+        }
+        pointA.x = e.targetTouches[0].pageX
+        pointA.y = e.targetTouches[0].pageY
+        pointB.x = e.targetTouches[1].pageX
+        pointB.y = e.targetTouches[1].pageY
+        juliB = Math.pow((Math.pow(Math.abs(pointA.x - pointB.x)) + Math.pow(Math.abs(pointA.y - pointB.y))), .5)
+    })
+
+    $box.on('touchend', e => {
+        if (e.targetTouches.length < 1) {
+            return false
+        }
+        if (juliB > juliA) {
+            alert('放大')
+        } else {
+            alert('缩小')
+        }
+    })
+})
+
+
