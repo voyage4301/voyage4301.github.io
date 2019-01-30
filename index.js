7ced303b577cf9454ae40255ddf9186a6778dbe0
voyage4301 <815717711@qq.com>
2019-01-30 17:48:21 +0800

    touch-move-demo10

--- index.js
+++ index.js
@@ -6,28 +6,30 @@ $(() => {
         juliA = 0,
         juliB = 0
     $box.on('touchstart', e => {
-        pointA.x = e.targetTouches[0].pageX
-        pointA.y = e.targetTouches[0].pageY
-        if (e.targetTouches.length > 1) {
-            pointB.x = e.targetTouches[1].pageX
-            pointB.y = e.targetTouches[1].pageY
+        console.log(e);
+        
+        pointA.x = e.changedTouches[0].pageX
+        pointA.y = e.changedTouches[0].pageY
+        if (e.changedTouches.length > 1) {
+            pointB.x = e.changedTouches[1].pageX
+            pointB.y = e.changedTouches[1].pageY
         }
         juliA = Math.pow((Math.pow(Math.abs(pointA.x - pointB.x), 2) + Math.pow(Math.abs(pointA.y - pointB.y), 2)), .5)
     })
 
     $box.on('touchmove', e => {
-        pointA.x = e.targetTouches[0].pageX
-        pointA.y = e.targetTouches[0].pageY
-        if (e.targetTouches.length > 1) {
-            pointB.x = e.targetTouches[1].pageX
-            pointB.y = e.targetTouches[1].pageY
+        pointA.x = e.changedTouches[0].pageX
+        pointA.y = e.changedTouches[0].pageY
+        if (e.changedTouches.length > 1) {
+            pointB.x = e.changedTouches[1].pageX
+            pointB.y = e.changedTouches[1].pageY
         }
         juliB = Math.pow((Math.pow(Math.abs(pointA.x - pointB.x), 2) + Math.pow(Math.abs(pointA.y - pointB.y), 2)), .5)
     })
 
     $box.on('touchend', e => {
         alert(juliA, juliB);
-        if (e.targetTouches.length > 1) {
+        if (e.changedTouches.length > 1) {
             if (juliB > juliA) {
                 alert('放大')
             } else {
